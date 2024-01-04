392e6643dec4e2ee233d2a071c655f8bb94ccd96
Lucas Aponso <s3896348@student.rmit.edu.au>
2024-01-04 18:01:28 +1100

    Added comments for enum, added design of SW for finite state machine.

--- include/sensorState.h
+++ include/sensorState.h
@@ -1,6 +1,6 @@
 enum class SensorState {
-    BOOTING,
-    RECIEVING,
-    RELAY,
-    CAPTURE
+    BOOTING, //state when the device boots up (inital state)
+    RELAY, //forwarding incoming messages to the next node (either sensor or gateway)
+    CAPTURE, //record the data from the GPIO pins (sensor data)
+    SENDING //sending the data from the CAPTURE state as a lora message to the next node (either sensor or gateway)
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,45 @@
 #include "sensorState.h"
 
-SensorState currentState = SensorState::BOOTING
+SensorState currentState = SensorState::BOOTING;
 
-void setup()
-{
+void setup() {
     Serial.begin(115200);
 }
 
-void loop() 
-{
-    switch (currentState) 
-    {
-        case BridgeState::BOOTING:
-            //do something then change to new state
-            break;
-        
-        case BridgeState::RECIEVING:
-            //do something then change to new state
+void loop() {
+    switch (currentState) {
+        case SensorState::BOOTING:
+            // Perform actions for BOOTING state
+            Serial.println("Booting up...");
+            
+            // Transition to the next state (e.g., CAPTURE)
+            currentState = SensorState::CAPTURE;
             break;
 
-        case BridgeState::RELAY:
-            //do something then change to new state
+        case SensorState::CAPTURE:
+            // Perform actions for CAPTURE state
+            Serial.println("Capturing sensor data...");
+            
+            // Transition to the next state (e.g., SENDING)
+            currentState = SensorState::SENDING;
             break;
-        case BridgeState::CAPTURE: 
-        {
-            //do something then change to new state            
+
+        case SensorState::SENDING:
+            // Perform actions for SENDING state
+            Serial.println("Sending sensor data...");
+            
+            // Transition to the next state (e.g., RELAY or BOOTING)
+            currentState = SensorState::RELAY;
             break;
-        }
+
+        case SensorState::RELAY:
+            // Perform actions for RELAY state
+            Serial.println("Relaying messages...");
+            
+            // Transition to the next state (e.g., BOOTING or CAPTURE)
+            currentState = SensorState::BOOTING;
+            break;
     }
+
     delay(1000);
 }
