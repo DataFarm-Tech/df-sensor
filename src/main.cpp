ca20deeadff403040586e4ac708153fe130e5dc4
Lucas Aponso <s3896348@student.rmit.edu.au>
2024-01-05 11:35:50 +1100

    Added creation of mutex, checking if mutex has been created if not print error. If correct, create tasks and start the schduler.

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,18 @@
 void setup()
 {
     Serial.begin(115200);
+    
+    counterMutex = xSemaphoreCreateMutex();
+    if (counterMutex != NULL) 
+    {
+        xTaskCreate(incrementTask,"IncrementTask",10000,NULL,1,NULL);
+        xTaskCreate(decrementTask,"DecrementTask",10000,NULL,2,NULL);
+        vTaskStartScheduler();
+    } 
+    else 
+    {
+        Serial.println("Mutex creation failed!");
+    }
 }
 
 
