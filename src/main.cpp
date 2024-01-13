0326cd593187fdc9b7d460f676946ade327074c6
Lucas <s3896348@student.rmit.edu.au>
2024-01-13 12:40:14 +1100

    Threading complete. Needs testing with module mod.

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,13 @@
 #include <SPI.h>
 #include <iostream>
 #include <Arduino.h>
+#include "eeprom.h"
+#include <LoRa.h>
+
+#define ss 5
+#define rst 14
+#define dio0 2
+
 typedef struct {
     float moisture;
     float ph;
@@ -15,7 +22,7 @@ pthread_mutex_t queueMutex = PTHREAD_MUTEX_INITIALIZER;
 float getMoisture();
 float getPh();
 
-void* listen(void*);
+
 void* send(void*);
 void* capture(void*);
 
@@ -23,44 +30,19 @@ void setup()
 {
     Serial.begin(115200);
 
-    pthread_t th_listen;
     pthread_t th_capture;
     pthread_t th_send;
 
     delay(2000);
 
-    pthread_create(&th_listen, NULL, listen, NULL);
     pthread_create(&th_capture, NULL, capture, NULL);
     pthread_create(&th_send, NULL, send, NULL);
 
-    pthread_join(th_listen, NULL);
     pthread_join(th_capture, NULL);
     pthread_join(th_send, NULL);
 }
 
 void loop() {}
-
-void* listen(void*)
-{
-    data sensorData;
-    sensorData.nodeId = "7D2322";
-
-    while (1) 
-    {
-        sensorData.moisture = getMoisture();
-        sensorData.ph = getPh();
-
-        printf("Listening Moisture: %.2f, pH: %.2f, nodeId: %s\n", sensorData.moisture, sensorData.ph, sensorData.nodeId.c_str());
-
-        pthread_mutex_lock(&queueMutex);
-        globalQueue.push(sensorData);
-        pthread_mutex_unlock(&queueMutex);
-        delay(60000);
-    }
-}
-
-
-
 
 void* capture(void*)
 {
@@ -85,6 +67,15 @@ void* capture(void*)
 
 void* send(void*)
 {
+    LoRa.setPins(ss, rst, dio0);
+    
+    while (!LoRa.begin(915E6)) 
+    {
+        Serial.println(".");
+        delay(500);
+    }
+
+    LoRa.setSyncWord(0xF3);
     data recData;
     while (1) 
     {
@@ -92,17 +83,19 @@ void* send(void*)
         if (!globalQueue.empty()) 
         {
             recData = globalQueue.front();
-            // Convert the nodeId to a String before concatenation
-            // String message = "Peeked at element in the queue, sending via lora: " + String(recData.nodeId.c_str());
-            // Serial.println(message);
-            Serial.println("Sending lora packet through LORA module");
+            std::string message = std::to_string(recData.moisture) + "," + std::to_string(recData.ph) + "," + recData.nodeId;
+            LoRa.beginPacket();
+            LoRa.print(message.c_str());
+            LoRa.endPacket();
+            Serial.print("Sending LoRa packet: ");
+            Serial.println(message.c_str());
+        
             globalQueue.pop();
         }
         pthread_mutex_unlock(&queueMutex);
         delay(30000); // Simulate some work
     }
 }
-
 
 
 float getPh()
