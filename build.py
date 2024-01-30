cd15e0a4ea3ea3c0bad29621a36e67b93d5b8870
Lucas <s3896348@student.rmit.edu.au>
2024-01-30 20:56:58 +1100

    Added a limit to when the capture thread can add to queue. If the queue is greater than 10 it stops theaddition of the queue. Also addded comments.

--- build.py
+++ build.py
@@ -1,6 +1,8 @@
 # pre_build.py
 import requests
-nodeId = "7D854323"
+# print("Enter NODEID: ")
+# nodeId = input()
+nodeId = "7DUJHHD"
 url = "https://9527-220-245-234-225.ngrok-free.app/node/add/man"
 api_key = "8151D74D23320A035F6DC0717F54B978BE3A29100AE52E716170D8A84B939BAC5C14EE537D1174919F67BA916E29D4DB696E73B4296E5F6FD2BE16984B1D31F1"
 
