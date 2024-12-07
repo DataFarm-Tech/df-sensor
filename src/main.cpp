6e386604139aeb30a39bc5b3677237afd41c30e3
Lucas Aponso <s3896348@student.rmit.edu.au>
2024-12-07 08:08:51 +0000

    mod: added lora lib for init

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,33 @@
 #include <Arduino.h>
 #include <SPI.h>
+#include <RH_RF95.h>
 
 #include "config.h"
 #include "rs485_int.h"
 
+RH_RF95 rf95(RFM95_NSS, RFM95_DIO0);
+
 void setup(void)
 {
-    Serial.begin(115200);
+    Serial.begin(115200); //start serial
     Serial2.begin(RS485_BAUD, SERIAL_8N1, RS485_RX, RS485_TX);
     pinMode(RS485_RTS, OUTPUT);
     digitalWrite(RS485_RTS, LOW); // SET PIN NORMALLY LOW
+
+    while (!rf95.init())
+    {
+        printf("Lora not connecting\n");
+    }
+
+    if (!rf95.setFrequency(RF95_FREQ))
+    {
+        printf("incorrect frequency setup\n");
+        while(1);
+    }
+
+    rf95.setTxPower(23, false);
+    rf95.setModemConfig(RH_RF95::Bw125Cr48Sf4096);
+
     while (!Serial) 
     {
         // wait for serial
@@ -22,7 +40,8 @@ void loop(void) {
     static data rs485_data_struct;
     data* rs485_data = &rs485_data_struct;
 
-    if (readSensor(rs485_data)) {
+    if (readSensor(rs485_data)) 
+    {
         printf("Humidity: %.1f\n", rs485_data->humidity);
         printf("Temperature: %.1f\n", rs485_data->temperature);
         printf("Conductivity: %d\n", rs485_data->conductivity);
@@ -30,9 +49,22 @@ void loop(void) {
         printf("Nitrogen: %d\n", rs485_data->nitrogen);
         printf("Phosphorus: %d\n", rs485_data->phosphorus);
         printf("Potassium: %d\n", rs485_data->potassium);
-    } else {
+    } 
+    else 
+    {
         printf("Failed to read sensor\n");
     }
+
+    // Send the string data
+    if (rf95.send(rec_data_buffer, sizeof(rec_data_buffer)))  // Use strlen to get the actual length of the string
+    {
+        printf("SUCCESS\n");
+    }
+    else 
+    {
+        printf("error\n");
+    }
+    
     
     delay(5000);
 }
