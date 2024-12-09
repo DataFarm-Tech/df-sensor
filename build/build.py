d494b7ff436e025b9731ff4373b7e0a118216e99
Lucas Aponso <s3896348@student.rmit.edu.au>
2024-12-09 04:19:14 +0000

    mod: copied build stage script from controller

--- build/build.py
+++ build/build.py
@@ -1,45 +1,64 @@
+Import("env")
+# This fixes the infinite loop problem when flashing the ESP32 with the Arduino framework on Windows
+if env.IsIntegrationDump():
+    Return()
+
 import requests
 import uuid
 import json
 from utils import get_id, get_device_type, device_type
 from enum import Enum
 
-Import("env")
-
-# fixes the infinite configuration loop issue
-if env.IsIntegrationDump():
-    Return()
-
 headers = {
     "Content-Type": "application/json"
 }
 
+ssid: str = ""
+password: str = ""
+api_key: str = ""
+
 device: device_type = get_device_type()
 id: str = get_id()
-api_key = None
+ota_enabled: int = 0  # Initially, OTA is disabled
 
 if device == device_type.CONTROLLER:
-    api_key: str = uuid.uuid4()
-    print("Controller", id)
     url = "https://194.195.255.90:443/man/add/controller"
+    api_key = uuid.uuid4()
     data = {"controller_id": id, "api_key": str(api_key)}
     response = requests.post(url, json=data, headers=headers, verify=False)
-
-    env.Append(CPPDEFINES=[
-    ("ID", env.StringifyMacro(id)),
-    ("OTA_ENABLED", 0),  # OTA_ENABLED is set to false
-    ("API_KEY", env.StringifyMacro(str(api_key)))])
-
-else:
-    print("Node", id)
-    url = "https://194.195.255.90:443/man/add/node"
-    data = {"nodeId": id}
-    response = requests.post(url, json=data, headers=headers, verify= False)
     
-    env.Append(CPPDEFINES=[("ID", env.StringifyMacro(id))])
+    # Ask the user if they would like to enable OTA
+    print("Would you like to enable OTA? (yes/no): ")
+    ota_choice = input().strip().lower()
+    
+    if ota_choice == "yes":
+        ota_enabled = 1  # Enable OTA
+        print("OTA enabled.")
+    else:
+        print("OTA not enabled.")
+    
+    print("Enter SSID: ")
+    ssid: str = input()
+    print("Enter Password: ")
+    password: str = input()
 
-# if response.status_code != 201:
-#     print(json.dumps(data, indent=4))
-#     exit()
+elif device == device_type.SENSOR:
+    url = "https://194.195.255.90:443/man/add/sensor"
+    data = {"node_id": id}
+    response = requests.post(url, json=data, headers=headers, verify=False)
 
-#  Add error handling / overwrite capability
+# Print the data being sent for debugging purposes
+print(json.dumps(data, indent=4))
+
+if response.status_code == 409:
+    print(f"{id} already exists.")
+    exit()
+
+# Append the configuration to the environment for the build process
+env.Append(CPPDEFINES=[
+    ("ID", env.StringifyMacro(id)),
+    ("SSID_CRED", env.StringifyMacro(ssid)),
+    ("PASS_CRED", env.StringifyMacro(password)),
+    ("OTA_ENABLED", ota_enabled),  # OTA_ENABLED is set based on user's choice
+    ("API_KEY", env.StringifyMacro(str(api_key)))
+])
