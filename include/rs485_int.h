39777c5945826aab88f673547ebc72010da9a3d8
Lucas Aponso <s3896348@student.rmit.edu.au>
2024-12-09 04:20:16 +0000

    mod: moved struct to types, changed parameter to array

--- include/rs485_int.h
+++ include/rs485_int.h
@@ -9,19 +9,7 @@
 extern uint8_t read_data_msg[];
 extern uint8_t rec_data_buffer[REC_DATA_LEN];
 
-// Structure to store processed data
-typedef struct {
-    float humidity; // Humidity value (e.g., 12.3)
-    float ph;       // pH value (e.g., 45.0)
-    float conductivity;
-    float temperature;
-    float nitrogen;
-    float phosphorus;
-    float potassium; 
-} data;
-
 // Function to process received message
-bool process_rs485_msg(uint8_t rec_data[REC_DATA_LEN], data *rs485_data);
-bool readSensor(data* rs485_data, int maxRetries = 3);
+bool read_sensor(uint8_t* rs485_data);
 
 #endif // RS485_INT_H
